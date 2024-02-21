119e5cf8e7013848beae221603ce467e2cd25ac5
Aashish Kumar Ray <2002raysi@gmail.com>
2024-02-21 19:11:12 +0530

    HTML Pratice

---
+++ JavaScript/datatypes.js
@@ -0,0 +1,35 @@
+//Numbers
+let x = 3.5;
+let y = 89;
+console.log(x, "\n", y)
+
+//String
+let lastName = "Ray";
+let firstName = "Aashish";
+console.log(firstName , lastName);
+
+//Boolean
+let a = true;
+let b = false;
+console.log(a, b);
+
+//undefined
+let p;
+if(p === undefined){
+    console.log("p is Undefined");
+}
+else{
+    console.log("p is Defined");
+}
+
+//Objects
+let obj = {firstName:"Aashish", lastName:"Ray"};
+console.log(obj);
+
+//arrays object
+let arr = ["Aashish", "Kanxu", obj, p, a, b, x, y];
+console.log(arr);
+
+//date object
+let date = new Date("2024-02-19");
+console.log(date);
---
+++ JavaScript/variable.js
@@ -0,0 +1,7 @@
+// var name = "Aashish";
+// name = "Aashish Ray";
+// let name = "Aashish";
+// name = "Aashish Ray";
+const name = "Aashish";
+// name = "Aashish Ray";
+console.log(name)
